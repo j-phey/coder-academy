e99772b57f57781e4f6c299c3c949c277dbe98d1
jjjjjjpppppp <jonphey91@gmail.com>
2023-10-16 14:24:02 +1100

    created a way to transfer inventories

--- python/composition/main.py
+++ python/composition/main.py
@@ -4,16 +4,18 @@ aragorn = rpg.Character('Aragorn', 'Human')
 galadriel = rpg.Character('Galadriel', 'Elf')
 frodo = rpg.Character('Frodo', 'Hobbit')
 
-# frodo.gold = 10
-# frodo.silver = 
-
-frodo.set_currency(9, 47, 23)
+frodo.inv.set_currency(9, 47, 23)
 
 chest = rpg.Chest(['longsword', 'iron helm'], 2, 25, 50)
 
-print(chest.__dict__)
+# print(chest.inv.__dict__)
 # print(aragorn.__dict__)
 # print(frodo.__dict__)
 # print(galadriel.__dict__)
 
-print(frodo.get_currency())
+# Frodo loots a chest!
+chest.inv.transfer(frodo.inv)
+
+print(f"Frodo's current inventory: {frodo.inv.__dict__}")
+print(f"Frodo's current gold/silver/copper breakdown: {frodo.inv.get_currency()}")
+print(f"Contents of chest: {chest.inv.__dict__}")
--- python/composition/rpg.py
+++ python/composition/rpg.py
@@ -3,10 +3,34 @@ class Character:
     def __init__(self, name, race):
         self.name = name
         self.race = race
-        # self.gold = 0
-        # self.silver = 0
-        self.copper = 0
+        # self.copper = 0
+        self.inv = Inventory([], 0, 0, 0)
+
+class Chest:
+
+    def __init__(self, items, gold, silver, copper):
+        # Chest in itself "has a" Inventory
+        self.inv = Inventory(items, gold, silver, copper) #self.inv is attribute, Inventory() is an instance of another class
+
+class Inventory:
     
+    def __init__(self, items, gold, silver, copper):
+        self.items = items # list
+        # self.gold = gold
+        # self.silver = silver
+        # self.copper = copper 
+        self.set_currency(gold, silver, copper) # Delegation
+    
+    def transfer(self, to_inv): # 'transfer' better to use than 'loot' as it's multipurpose
+        # Add all the items from from_inv to to_inv
+        to_inv.items.extend(self.items)
+        # Delete all the items from the from_inv
+        self.items = []
+        # Add the currency from from_inv to to_inv
+        to_inv.copper += self.copper
+        # Set currency of from_inv to 0
+        self.copper = 0
+
     # This is called a 'setter'
     def set_currency(self, gold, silver, copper):
         self.copper = gold * 10000 + silver * 100 + copper
@@ -17,11 +41,3 @@ class Character:
         silver = (self.copper % 10000) // 100
         copper = self.copper % 100
         return gold, silver, copper
-
-class Chest:
-
-    def __init__(self, items, gold, silver, copper):
-        self.items = items
-        self.gold = gold
-        self.silver = silver
-        self.copper = copper
