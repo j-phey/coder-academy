694f916cdc68642aacb58fc7970a06959bf15f2b
jjjjjjpppppp <jonphey91@gmail.com>
2023-11-14 14:21:35 +1100

    Added paramaters into RESTful API

--- flask-lessons/basic/main.py
+++ flask-lessons/basic/main.py
@@ -15,18 +15,41 @@ def spam(): # Define a handler function to handle the route
     # Flask packages up this route and provides the basic HTTP headers, etc.
     # Flask JSON-ifies the object
 
-@app.route('/hello') # Request: GET http://127.0.0.1:5000/hello?name=Matt
-def hello():
-    # print(request.headers) # We can get the request headers
-    name = request.args.get('foo') # GET http://127.0.0.1:5000/hello?foo=Jon now sets the name as Jon
+# @app.route('/hello') # Request: GET http://127.0.0.1:5000/hello?name=Matt
+# def hello():
+#     # print(request.headers) # We can get the request headers
+#     name = request.args.get('name') # GET http://127.0.0.1:5000/hello?name=Jon now sets the name as Jon
+#     # name = 'Jack'
+#     return { 'message': f'Hello, {name}!' }
+
+# THIS IS HOW YOU CAN RECEIVE RESTful API PARAMETERS
+@app.route('/hello/<name>') # Request: GET http://127.0.0.1:5000/hello/blahblah
+def hello(name): # Whatever you call the <>, you need it in the function as a parameter
+    # name = request.args.get('name') # GET http://127.0.0.1:5000/hello?name=Jon now sets the name as Jon
     # name = 'Jack'
     return { 'message': f'Hello, {name}!' }
 
-@app.route('/add') # Request: GET http://127.0.0.1:5000/add?num1=10&num2=24
-def add():
-    num1 = int(request.args.get('num1')) # We need int() because URLs are always strings
-    num2 = int(request.args.get('num2'))
-    return { 'result': num1 + num2 }
+# GET http://127.0.0.1:5000/add/12/39
+@app.route('/add/<int:num1>/<int:num2>') # Telling flask these are int
+# Specifying data type also handles the error handling for you
+def add(num1, num2):
+        # num1 = int(request.args.get('num1')) 
+        # num2 = int(request.args.get('num2'))
+        return { 'result': num1 + num2 }
+    # We don't need the try/except here because <int:num1> handles errors
+
+# @app.route('/add') # Request: GET http://127.0.0.1:5000/add?num1=10&num2=24
+# def add():
+#     try:
+#         num1 = int(request.args.get('num1')) # We need int() because URLs are always strings
+#         num2 = int(request.args.get('num2'))
+#         return { 'result': num1 + num2 }
+#     except TypeError:
+#         return { 'error': 'num1 and num2 must be int' }, 400 # Error handling for cases when num1 and num2 are not int
+
+@app.errorhandler(TypeError) # Error handling for cases when num1 and num2 aren't provided
+def type_error(error):
+    return { 'error': str(error) }, 400
 
 # The default 401 Error is in HTML. Flask must always return a JSON result, therefore
 @app.errorhandler(404)
