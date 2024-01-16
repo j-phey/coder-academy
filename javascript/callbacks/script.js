35549f05cd910ffe302cd967dd950890c26b40a9
jjjjjjpppppp <jonphey91@gmail.com>
2024-01-16 14:22:54 +1100

    Created an API request for Dad Joke, including a callback

---
+++ javascript/callbacks/script.js
@@ -0,0 +1,37 @@
+// // A basic callback function
+
+// function adder(x, y, cb) {
+//     cb(x + y)
+// }
+
+// adder(5, 10, result => console.log(result))
+
+// console.log('done')
+
+// --------------
+
+// API request for Dad Joke. Asynchronous function using a cb
+
+function getJoke(cb) { // Callback
+    // 2. Create the XHR
+    const req = new XMLHttpRequest()
+    // 3. Add listener for when the response is received and parsed
+    // 9. Listener callback is triggered, which is turn calls cb, passing the joke
+    req.addEventListener('load', event => cb(event.target.response.joke))
+    // 4. Open the URL with the appropriate verb
+    req.open('GET', 'https://icanhazdadjoke.com/')
+    // 5. Set Accept header so server give sus JSON
+    req.setRequestHeader('Accept', 'application/json')
+    // 6. Set responseType so XHR object parses the JSON
+    req.responseType = 'json'
+    // 7. Send the request, then immediately return from getJoke (i.e. don't wait!)
+    req.send()
+}
+
+// 1. Call getJoke and pass a callback function
+// 10. Callback is called via cb in the load listener in getJoke, receiving the joke
+getJoke(joke => console.log(joke))
+
+// 8. get Joke returned immediately, so log out message
+console.log('Request sent!')
+
