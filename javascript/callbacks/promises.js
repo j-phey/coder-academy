941a27babd5b9a70cfb49acc14eee113e4db0003
jjjjjjpppppp <jonphey91@gmail.com>
2024-01-17 12:31:07 +1100

    Added a promise through promises.jy

---
+++ javascript/callbacks/promises.js
@@ -0,0 +1,44 @@
+// const x = 2
+// const y = 'asd'
+
+function adder(a, b) {
+    return a + b
+}
+
+function adderPromise(x,y) {
+
+    // const calc = new Promise((resolve, reject) => {
+    return new Promise((resolve, reject) => {
+        if (typeof x === 'number' && typeof y === 'number') { // Only if the two variables are numbers, do the promise
+            const answer = adder(x,y)
+            resolve(answer)
+        } else { // Else, reject it
+            reject('x and y must be numbers')
+        }
+    })
+}
+
+// console.log(calc)
+// calc 
+
+// Adds 3 values 
+
+// adderPromise(10, 20)
+//     .then(value => {
+//         adderPromise(value, 100)
+//             .then(answer => console.log(answer))
+//     })
+//     .catch(err => console.error(err))
+
+// Another way of doing it...
+adderPromise(10, 20)
+    .then(value => adderPromise(value, 100))
+    .then(answer => console.log(answer))
+    .catch(err => console.error(err))
+
+
+// adderPromise(150, 300)
+//     .then(value => console.log(value))
+//     .catch(err => console.error(err)) 
+
+console.log('Not waiting for resolve!')
