f2b71933a418ad34d407fff2a6f91f86d2c318b3
jjjjjjpppppp <jonphey91@gmail.com>
2024-02-05 22:37:29 +1100

    Added the Bulma CSS framework for drop down menu

---
+++ javascript/journal/src/components/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Home from './Home'
+import CategorySelection from './CategorySelection'
+import NewEntry from './NewEntry'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import NavBar from './NavBar'
+
+const App = () => {
+  return (
+    <>
+      
+      <BrowserRouter> 
+        <NavBar />
+        <h1>Journal</h1>
+        <Routes>
+          <Route path='/' element={<p>Hello</p>} />
+          <Route path='/category' element={<CategorySelection />} />
+          <Route path='/entry'>
+            <Route path='new' element={<NewEntry />} />
+          </Route>
+          <Route path='*' element={<h3>Page not found</h3>} />
+        </Routes>
+      </BrowserRouter>
+      {/* <Home />
+      <CategorySelection />
+      <NewEntry /> */}
+    </>
+  )
+}
+
+export default App
