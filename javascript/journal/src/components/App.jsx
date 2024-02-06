e4a8e531693d6dc259dce1fcdf90d4b91e685f72
jjjjjjpppppp <jonphey91@gmail.com>
2024-02-06 15:22:09 +1100

    Refactored NewEntry; created addEntry in App

--- javascript/journal/src/components/App.jsx
+++ javascript/journal/src/components/App.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Home from './Home'
 import CategorySelection from './CategorySelection'
 import NewEntry from './NewEntry'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes, useParams } from 'react-router-dom'
 import NavBar from './NavBar'
+import ShowEntry from './ShowEntry'
 
 const App = () => {
+  const [categories] = useState(["Food", "Gaming", "Coding", "Other"])
+  const [entries, setEntries] = useState([{category: 0, content: 'I like Pizza!'}])
+  const params = useParams()
+
+  function addEntry(cat_id, content) {
+      // 1. Create a entry object from user input
+      const newEntry = {
+          category: cat_id,
+          content: content,
+      }
+      // 2. Add new entry to the entries list
+      setEntries([...entries, newEntry])
+  }
+
+  // Higher Order Component (HOC)
+  function ShowEntryWrapper() {
+    const { id } = useParams()
+    return <ShowEntry entry={entries[id]} />
+  }
+
   return (
     <>
       
@@ -14,9 +35,11 @@ const App = () => {
         
         <Routes>
           <Route path='/' element={<p>Hello</p>} />
-          <Route path='/category' element={<CategorySelection />} />
-          <Route path='/entry'>
-            <Route path='new/:cat_id' element={<NewEntry />} />
+          <Route path='/category' element={<CategorySelection categories={categories}/>} />
+          <Route path='/entry'> 
+            <Route path=':id' element={<ShowEntryWrapper />} />
+            {/* Pass in the categories array for the cat id to become the cat name in the front end i.e. for 'New entry in category Gaming' instead of 'New entry in category 1'*/}
+            <Route path="new/:cat_id" element={<NewEntry categories={categories} addEntry={addEntry} />} /> 
           </Route>
           <Route path='*' element={<h3>Page not found</h3>} />
         </Routes>
--- javascript/journal/src/components/NewEntry.jsx
+++ javascript/journal/src/components/NewEntry.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useParams } from 'react-router-dom'
 
-const NewEntry = () => {
+const NewEntry = ({ categories, addEntry }) => { // pass categories as a prop
     const params = useParams()
-    console.log(params)
+    const [entry, setEntry] = useState('')
   
-    return <h3>New entry in category {params.cat_id}</h3>
-}
+    function createEntry(e) {
+        e.preventDefault()
+        // Create a new entry
+        addEntry(params.cat_id, entry)
+        // 3. Clear input textarea
+        setEntry('')
+    }
+
+    return (
+    <>
+    {/* // Categories as a prop, cat.id as an index - renders on front end as 'New entry in category Gaming' */}
+    <h3>New entry in category {categories[params.cat_id]}</h3> 
+    {/* form>(textarea+button) */}
+    {/* preventDefault prevents reload on button press */}
+    <form className="section" onSubmit={createEntry}>
+        <div className="field">
+            <label className="label">Content</label>
+            <div className="control">
+                <textarea className="textarea" value={entry} onChange={e => setEntry(e.target.value)} placeholder="Type here"></textarea>
+            </div>
+        </div>
+
+        <div className="field is-grouped">
+            <div className="control">
+                <button className="button is-link">Create Entry</button>
+            </div>
+        </div>
+    </form>
+    </>
+)}
 
 export default NewEntry
