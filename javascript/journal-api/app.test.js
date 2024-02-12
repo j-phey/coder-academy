714a9cbe544fa09eb8d89ddb958d0ac548a4a5b8
jjjjjjpppppp <jonphey91@gmail.com>
2024-02-12 13:28:43 +1100

    Finished Express testing

--- javascript/journal-api/app.test.js
+++ javascript/journal-api/app.test.js
@@ -10,43 +10,66 @@ describe("App Test", () => {
         expect(res.body.info).toBe('Journal API') // Make sure it contains the text expected
     })
 
-    test('POST /entries', async () => {
-        const cats = await request(app).get('/categories')
-        const res = (await request(app).post('/entries')).send({
-            category: cats[0]._id,
+    
+
+    describe('POST /entries', () => {
+        let cats, res
+    
+        beforeAll(async () => {
+          cats = await request(app).get('/categories')
+          res = await request(app).post('/entries').send({
+            category: cats.body[0]._id,
             content: 'Jest test content'
+          })
         })
 
-        expect(res.status).toBe(201)
-        expect(res.header['content-type']).toContain('json')
+        test('Returns JSON with 201 status', () => {
+            expect(res.status).toBe(201)
+            expect(res.header['content-type']).toContain('json')
+        })
+      
+        test('Body has _id, category and content fields', () => {
+            expect(res.body._id).toBeDefined()
+            expect(res.body.category).toBeDefined()
+            expect(res.body.content).toBeDefined()
+        })
 
-
-        // Cleanup
-        request(app).delete(`/entries/${res.body._id}`)
+        test('Category is an object with _id and name fields', () => {
+            expect(res.body.category).toBeInstanceOf(Object)
+            expect(res.body.category._id).toBeDefined()
+            expect(res.body.category.name).toBeDefined()
+          })
+      
+          test('Correct category and content are returned', () => {
+            expect(res.body.category._id).toBe(cats.body[0]._id)
+            expect(res.body.content).toBe('Jest test content')
+          })
+      
+          afterAll(() => {
+            // Cleanup
+            request(app).delete(`/entries/${res.body._id}`)
+          })
     })
 
     describe('GET /categories', () => {
-        let res 
-
-        beforeEach(async () => { // Put this before each expect() below
-            res = await request(app).get('/categories')
+        let res
+    
+        beforeAll(async () => {
+          res = await request(app).get('/categories')
         })
-
+        
         test('Returns JSON content', () => {
-            expect(res.status).toBe(200) // Check status for 200
-            expect(res.header['content-type']).toContain('json') // Checks for content type
-        })
-
-        test('Returns an array', () => {
+            expect(res.status).toBe(200)
+            expect(res.header['content-type']).toContain('json')
+          })
+          test('Returns an array', () => {
             expect(res.body).toBeInstanceOf(Array)
-        })
-
-        test('Array has 4 elements', () => {
+          })
+          test('Array has 4 elements', () => {
             expect(res.body).toHaveLength(4)
-        })
-
-        test('One element is an object with key "name" == "Food"', () => {
+          })
+          test('One element is an object with key "name" == "Food"', () => {
             expect(res.body).toEqual(expect.arrayContaining([expect.objectContaining({ name: "Food" })]))
+          })
         })
-    })
-})
+      })
